d70bc08a98aef6e64833124b9f97a23e1d9d9890
Stephen Baynham <sdbaynham@gmail.com>
2015-07-11 00:08:44 -0400

    Initial checkin of express+typescript

---
+++ routes/index.ts
@@ -0,0 +1,9 @@
+import express = require('express');
+var router : express.Router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req: express.Request, res : express.Response, next: Function) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
